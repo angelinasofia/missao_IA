2ecb6ed87420ba00c30cf201930eb1b61893666c
angelinasofia <160738220+angelinasofia@users.noreply.github.com>
2025-07-30 16:42:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,40 +8,76 @@ const perguntas = [
     {
        enunciado: "Se você fosse uma das bonecas bratz, preferia usar?",
        alternativas: [
-        "Vestido",
-        "Calça",
+        {
+          texto:"Vestido",
+          afirmacao: "Você é uma boneca mais delicada e com grande personalidade"
+        },
+        {
+         texto:"Calça",
+         afirmacao: "Você é uma boneca mais estilosa e com uma grande personalidade"
+        }
+        
        ]
     },
 
     {
         enunciado: "Você prefere usar?",
         alternativas: [
-         "Salto",
-         "Bota",
+         {
+            texto:"Salto",
+            afirmacao:"Com seus saltos altos e lindos, isso corresponde a Cloe que tem uma grande autoestima"
+         },
+         {
+            texto:"Bota",
+            afirmacao: "Com suas botas lindas e brilhantes, isso corresponde a Yasmin que é muito seletiva em suas escolhas"
+         }
+
         ]
      },
 
      {
         enunciado: "Você prefere?",
         alternativas: [
-         "Cabelos escuros",
-         "Cabelos claros",
+          {
+            texto:"Cabelos escuros",
+            afirmacao:"Jade tem seus lindos cabelos pretos e grandes, isso diz que você teria um cabelo que combina com qualquer tipo de estilo"
+          },
+          {
+            texto:"Cabelos claros",
+            afirmacao:"Cloe tem seus lindos cabelos loiros que combinam com seus olhos e seu estilo mais delicado"
+
+          }
+
         ]
      },
 
      {
         enunciado: "Você prefere usar?",
         alternativas: [
-         "Uma bolsa com alça maior",
-         "Uma bolsa com uma alça menor",
+         {
+            texto:"Uma bolsa com alça maior",
+            afirmacao:"Uma boneca muito estilosa e que gosta de variar suas cores de bolsa"
+         },
+         {
+            texto:"Uma bolsa com uma alça menor",
+            afirmacao:"Uma boneca muito estilosa e que gosta de variar suas cores de bolsa"
+         }
+
         ]
      },
 
      {
         enunciado: "Qual dessas duas bonecas você acha mais bonita?",
         alternativas: [
-         "Jade",
-         "Cloe",
+         {
+            texto:"Jade",
+            afirmacao:"Jade, com seus olhos castanhos apaixonam qualquer um além de tudo ela ainda é cantora de música pop"
+         },
+         {
+            texto:"Cloe",
+            afirmacao:"Cloe, com seus olhos castanhos apaixonam qualquer um além disso ela é uma grande patricinha com bom gosto"
+         }
+      
         ]
      }
 
@@ -53,6 +89,19 @@ let perguntaAtual;
 function mostraPergunta (){
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas (){
+   for (const alternativa of perguntaAtual.alternativas){
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativas.textContent = alternativa.texto; 
+      botaoAlternativas.addEventListener("click", function () {
+         atual++;
+         mostraPergunta();
+      })
+      caixaAlternativas.appendChild(botaoAlternativas);
+   }
 }
 
 mostraPergunta();
